c3f3d73ee7582f55dd50877749391bceb4fe6549
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-02-11 21:40:48 +0330

    Add README.md and LICENSE.txt.

---
+++ MMSApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MMSApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
