14f86cc4582aba1547ab3638d8274cc037887887
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-13 09:44:42 +0330

    generate city

---
+++ MMSApp/Controllers/CitiesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MMSApp.Models;
+
+namespace MMSApp.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly MMSPortalV3Context _context;
+
+        public CitiesController(MMSPortalV3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Cities
+        public async Task<IActionResult> Index()
+        {
+              return _context.Cities != null ? 
+                          View(await _context.Cities.ToListAsync()) :
+                          Problem("Entity set 'MMSPortalV3Context.Cities'  is null.");
+        }
+
+        // GET: Cities/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // GET: Cities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProvinceId,Name,EnglishName,TosanCodeNew,OldCode,IsDeleted,IsActive,CreatorUserId,CreateDate,UpdaterUserId,UpdateTime")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(city);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,ProvinceId,Name,EnglishName,TosanCodeNew,OldCode,IsDeleted,IsActive,CreatorUserId,CreateDate,UpdaterUserId,UpdateTime")] City city)
+        {
+            if (id != city.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(city);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CityExists(city.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.Cities
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Cities == null)
+            {
+                return Problem("Entity set 'MMSPortalV3Context.Cities'  is null.");
+            }
+            var city = await _context.Cities.FindAsync(id);
+            if (city != null)
+            {
+                _context.Cities.Remove(city);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CityExists(long id)
+        {
+          return (_context.Cities?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
