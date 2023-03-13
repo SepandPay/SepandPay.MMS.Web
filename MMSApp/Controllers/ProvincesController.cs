df1d3edf8eaf6926623f06804fadc544c5231cab
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-03-13 09:58:19 +0330

    generate Province

---
+++ MMSApp/Controllers/ProvincesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MMSApp.Models;
+
+namespace MMSApp.Controllers
+{
+    public class ProvincesController : Controller
+    {
+        private readonly MMSPortalV3Context _context;
+
+        public ProvincesController(MMSPortalV3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Provinces
+        public async Task<IActionResult> Index()
+        {
+              return _context.Provinces != null ? 
+                          View(await _context.Provinces.ToListAsync()) :
+                          Problem("Entity set 'MMSPortalV3Context.Provinces'  is null.");
+        }
+
+        // GET: Provinces/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Provinces == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Provinces
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return View(province);
+        }
+
+        // GET: Provinces/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Provinces/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Code,Name,Abbreviation,TelCode,CountryId,IsDeleted,IsActive,CreatorUserId,CreateDate,UpdaterUserId,UpdateTime")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(province);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(province);
+        }
+
+        // GET: Provinces/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Provinces == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Provinces.FindAsync(id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+            return View(province);
+        }
+
+        // POST: Provinces/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Code,Name,Abbreviation,TelCode,CountryId,IsDeleted,IsActive,CreatorUserId,CreateDate,UpdaterUserId,UpdateTime")] Province province)
+        {
+            if (id != province.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(province);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProvinceExists(province.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(province);
+        }
+
+        // GET: Provinces/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Provinces == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Provinces
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return View(province);
+        }
+
+        // POST: Provinces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Provinces == null)
+            {
+                return Problem("Entity set 'MMSPortalV3Context.Provinces'  is null.");
+            }
+            var province = await _context.Provinces.FindAsync(id);
+            if (province != null)
+            {
+                _context.Provinces.Remove(province);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProvinceExists(long id)
+        {
+          return (_context.Provinces?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
